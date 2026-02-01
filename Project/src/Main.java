a19edeace628a02d4e2ad726277198af8dba41c4
Kushipo <eeindemini@gmail.com>
2026-02-02 00:36:46 +0100

    Up til User Input

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,10 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.print("A me piace la pizza!\n");
 
-        int age = 98;
+        /*int age = 98;
 
         System.out.println(age);
+
+        boolean isYear = false;
+
+        if(age == 98) {
+            isYear = true;
+        }
+
+        if(isYear) {
+            System.out.println(age + " is correct!");
+            System.out.println("Status : " + isYear);
+        } else {
+            System.out.println(age + " is NOT correct!");
+            System.out.println("Status : " + isYear);
+        }
+        */
+
+        // use case example of input (Scanner library)
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter width(cm) : ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter heigth(cm) : ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+        System.out.println("Area : " + area + " cm^2");
     }
 }
